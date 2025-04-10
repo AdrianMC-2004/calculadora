85aa9e7f2b0907a9cb4dc05874d444e0bbbcc061
Raphael <rmraphaelramos@gmail.com>
2025-04-10 13:02:32 -0500

    changemultiplicar

--- funcion2.js
+++ funcion2.js
@@ -2,5 +2,5 @@ function multiplicar() {
     var numero1 = parseFloat(document.getElementById('num1').value);
     var numero2 = parseFloat(document.getElementById('num2').value);
     var multiplicar = numero1 * numero2;
-    document.getElementById('Resultado:').innerText = division;
+    document.getElementById('resultado:').innerText = division;
 }
